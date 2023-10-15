9bd5e7a328abb61ee07aec1da274bca11c99d674
Erim Erkin Doğan <erimerkin@gmail.com>
2023-10-15 23:40:40 +0300

    Added 404 page for backend & fixed urls

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, {Request, Response} from "express";
 
 const app = express();
 
@@ -8,6 +8,13 @@ app.use(express.json());
 
 app.use('/static', express.static('public'))
 app.use('/api', playlistRouter)
+
+app.use('*', (req: Request, res: Response) => {
+  res.status(404).json({
+    status: 404,
+    message: "The resource you are looking for does not exist",
+  });
+});
 
 app.listen(1955, () => {
   console.log("Server is listening on port 1955");
--- backend/src/services/playlist.service.ts
+++ backend/src/services/playlist.service.ts
@@ -1,9 +1,9 @@
 import { PlaylistTrack } from "../models/playlistTrack.model";
 
 const playlist: PlaylistTrack[] = [
-    new PlaylistTrack("Yoga", "image", "/static/images/yoga.jpg", 5),
-    new PlaylistTrack("Earth", "video", "/static/videos/earth.mp4", 30),
-    new PlaylistTrack("Bench", "image", "/static/images/bench.jpg", 5),
+    new PlaylistTrack("Yoga", "image", "http://localhost:1955/static/images/yoga.jpg", 5),
+    new PlaylistTrack("Earth", "video", "http://localhost:1955/static/videos/earth.mp4", 30),
+    new PlaylistTrack("Bench", "image", "http://localhost:1955/static/images/bench.jpg", 5),
 ];
 
 function getPlaylist() : PlaylistTrack[]{
